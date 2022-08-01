f87faae16a5037e32668cde266449301cd5aa0a4
unknown <duongducchinh160@gmail.com>
2022-08-01 16:06:20 +0700

    Thiet ke vaf trien khai lop Triangle

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape("black",false);
+        System.out.println(shape);
+        Triangle triangle = new Triangle("yellow",false,5,6,7);
+        System.out.println(triangle);
+    }
+}
